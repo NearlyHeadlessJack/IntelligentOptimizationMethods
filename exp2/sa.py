3cc8282a901a9a10e6a811fb2cba71b3b8c40815
MACOS <jwang0702@foxmail.com>
2022-05-15 00:08:53 +0800

    程序入口

---
+++ exp2/sa.py
@@ -0,0 +1,29 @@
+import numpy as np
+from search import Search
+"""
+模拟退火实验作业
+人工智能1903班
+王萱
+20195235
+"""
+
+
+
+def main():
+    search = Search(temp0=300,temp=400,r=0.98,deltaT=4,isDelta=True,inner_count=5)
+    search.search()
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+if __name__ == '__main__':
+    main()
